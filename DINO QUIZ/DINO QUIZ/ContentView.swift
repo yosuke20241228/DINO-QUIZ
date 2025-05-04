58a70aa5a3905fbda6848a001994da9e1c9e644f
yosuke20241228 <yosuke_1999@icloud.com>
2025-05-04 18:39:19 +0900

    create Dino QUIZ application to the Github
    Yeah. yosuke, Me"david" did it for you today.

---
+++ DINO QUIZ/DINO QUIZ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DINO QUIZ
+//
+//  Created by 山﨑　陽介 on 2025/05/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DINO QUIZ/DINO QUIZ/DINO_QUIZApp.swift
@@ -0,0 +1,17 @@
+//
+//  DINO_QUIZApp.swift
+//  DINO QUIZ
+//
+//  Created by 山﨑　陽介 on 2025/05/04.
+//
+
+import SwiftUI
+
+@main
+struct DINO_QUIZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
