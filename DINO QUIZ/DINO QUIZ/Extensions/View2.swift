52dba615cfc2fd9dbdb60a5ac0cac0194fca666e
yosuke20241228 <yosuke_1999@icloud.com>
2025-05-09 06:19:53 +0900

    ボタンイラスト変更。背景変更。問題作成。

---
+++ DINO QUIZ/DINO QUIZ/Extensions/View2.swift
@@ -0,0 +1,18 @@
+//
+//  View2.swift
+//  DINO QUIZ
+//
+//  Created by 山﨑　陽介 on 2025/05/09.
+//
+
+import SwiftUI
+
+struct View2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    View2()
+}
