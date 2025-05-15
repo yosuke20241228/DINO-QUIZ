af8aaa90c7b67762be657103082c55e6bac05b2b
yosuke20241228 <yosuke_1999@icloud.com>
2025-05-16 04:59:18 +0900

    広告をつける

---
+++ DINO QUIZ/DINO QUIZ/Extensions/RewardAdButton.swift
@@ -0,0 +1,18 @@
+//
+//  RewardAdButton.swift
+//  DINO QUIZ
+//
+//  Created by 山﨑　陽介 on 2025/05/16.
+//
+
+import SwiftUI
+
+struct RewardAdButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RewardAdButton()
+}
---
+++ DINO QUIZ/DINO QUIZ/RewardAdViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  RewardAdViewModel.swift
+//  DINO QUIZ
+//
+//  Created by 山﨑　陽介 on 2025/05/12.
+//
+
+import SwiftUI
+
+struct RewardAdViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RewardAdViewModel()
+}
