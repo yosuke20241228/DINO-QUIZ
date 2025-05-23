6733c6c2e107a611d8250b60ff14ed6561cd3d03
yosuke20241228 <yosuke_1999@icloud.com>
2025-05-23 13:50:34 +0900

    新たにlookbackviewを作成し広告視聴後に問題を振り返れるようにコーディング

---
+++ DINO QUIZ/DINO QUIZ/Extensions/RewardLaunchButton2.swift
@@ -0,0 +1,18 @@
+//
+//  RewardLaunchButton2.swift
+//  DINO QUIZ
+//
+//  Created by 山﨑　陽介 on 2025/05/23.
+//
+
+import SwiftUI
+
+struct RewardLaunchButton2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RewardLaunchButton2()
+}
